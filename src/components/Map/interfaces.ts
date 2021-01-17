62a7b0662559e83368c4c5fc23385c8eeca421f5
otaviobonder <otaviobonder@gmail.com>
2021-01-17 19:45:12 -0300

    fix indentation

--- src/components/Map/interfaces.ts
+++ src/components/Map/interfaces.ts
@@ -1,34 +1,34 @@
 export interface IData {
-    alias?: string
-    publicKey: string
-    color: string
-    lat?: number
-    lng?: number
+  alias?: string;
+  publicKey: string;
+  color: string;
+  lat?: number;
+  lng?: number;
 }
 
 export interface IProps {
-    data: IData[]
-    height: number
+  data: IData[];
+  height: number;
 }
 
 export interface IPoints {
-    alias: string
-    publicKey: string
-    color: string
-    lat: number
-    lng: number
+  alias: string;
+  publicKey: string;
+  color: string;
+  lat: number;
+  lng: number;
 }
 
 export interface INodeInfo {
-    publicKey: string;
-    capacityRatio: number;
+  publicKey: string;
+  capacityRatio: number;
 }
 
 export interface INodePairs extends INodeInfo {
-    alias: string
-    color: string[]
-    endLat: number
-    endLng: number
-    startLat: number
-    startLng: number
+  alias: string;
+  color: string[];
+  endLat: number;
+  endLng: number;
+  startLat: number;
+  startLng: number;
 }
