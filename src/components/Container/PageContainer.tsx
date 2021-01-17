66430261321f07c1fc3d5c46e1acac1f945347c7
otaviobonder <otaviobonder@gmail.com>
2021-01-17 19:04:34 -0300

    created page container component

---
+++ src/components/Container/PageContainer.tsx
@@ -0,0 +1,16 @@
+import React from 'react';
+import { Container, ContainerProps, makeStyles } from '@material-ui/core';
+
+const useStyles = makeStyles({
+  container: {
+    marginTop: 30,
+  },
+});
+
+export const PageContainer: React.FC<ContainerProps> = ({ children, maxWidth }) => {
+  const classes = useStyles();
+
+  return (
+    <Container className={classes.container} maxWidth={maxWidth}>{children}</Container>
+  );
+};
