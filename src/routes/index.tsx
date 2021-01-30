dfeea3c58509fdd2e43be16dc5d1e3bda96d9a5d
otaviobonder <otaviobonder@gmail.com>
2021-01-29 23:50:04 -0300

    lazy load components

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -1,16 +1,15 @@
-import React from "react";
+import React, { Suspense, lazy } from "react";
 import { Switch, Route } from "react-router-dom";
 import { makeStyles } from "@material-ui/core";
 import { Home } from "../pages/home";
 import { Navigation } from "../components/Navigation";
-import { Explorer } from "../pages/explorer";
 import { WatchTower } from "../pages/watchtower";
 import { LiquidityProvider } from "../pages/liquidity";
-import { Comparison } from "../pages/comparison";
 import ErrorBoundary from "../components/ErrorBoundary";
 import { Footer } from "../components/Footer";
 import { NotFound } from "../pages/notFound";
 import { useTracking } from "../hooks/useTracking";
+import { Loader } from "../components/Loader";
 
 const useStyles = makeStyles({
   body: {
@@ -23,10 +22,36 @@ const useStyles = makeStyles({
     flex: 1,
     justifyContent: "center",
   },
+  load: {
+    display: "flex",
+    flex: 1,
+    alignItems: "center",
+    justifyContent: "center",
+  },
 });
+
+const SuspenseFallback: React.FC = () => {
+  const classes = useStyles();
+
+  return (
+    <div className={classes.load}>
+      <Loader />
+    </div>
+  );
+};
 
 export const Routes: React.FC = () => {
   const classes = useStyles();
+
+  // lazy load
+  const Explorer = lazy(() =>
+    import("../pages/explorer").then((module) => ({ default: module.Explorer }))
+  );
+  const Comparison = lazy(() =>
+    import("../pages/comparison").then((module) => ({
+      default: module.Comparison,
+    }))
+  );
 
   // update page changes
   useTracking();
@@ -36,26 +61,28 @@ export const Routes: React.FC = () => {
       <Navigation />
       <ErrorBoundary>
         <div className={classes.components}>
-          <Switch>
-            <Route exact path="/">
-              <Home />
-            </Route>
-            <Route path="/explorer">
-              <Explorer />
-            </Route>
-            <Route path="/watchtower">
-              <WatchTower />
-            </Route>
-            <Route path="/liquidity">
-              <LiquidityProvider />
-            </Route>
-            <Route path="/comparison">
-              <Comparison />
-            </Route>
-            <Route path="*">
-              <NotFound />
-            </Route>
-          </Switch>
+          <Suspense fallback={SuspenseFallback}>
+            <Switch>
+              <Route exact path="/">
+                <Home />
+              </Route>
+              <Route path="/explorer">
+                <Explorer />
+              </Route>
+              <Route path="/watchtower">
+                <WatchTower />
+              </Route>
+              <Route path="/liquidity">
+                <LiquidityProvider />
+              </Route>
+              <Route path="/comparison">
+                <Comparison />
+              </Route>
+              <Route path="*">
+                <NotFound />
+              </Route>
+            </Switch>
+          </Suspense>
         </div>
       </ErrorBoundary>
       <Footer />
